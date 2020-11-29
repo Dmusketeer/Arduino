86f471c26842df3ffb264042c7f458b0e1044b4f
Dheeraj Tiwari <tdheeraj9a@gmail.com>
2020-11-29 18:37:31 +0530

    led blinking Program
    led is connected at port number 12.

---
+++ LedBlink.ino
@@ -0,0 +1,16 @@
+
+
+void setup()
+{
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);
+  delay(100); // Wait for 1000 millisecond(s)
+  // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);
+  delay(100); // Wait for 1000 millisecond(s)
+}
